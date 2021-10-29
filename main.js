9d65cddd933dc35203cb03c1926156f5d7f71ebf
Ami <86639013+serpentfang2550@users.noreply.github.com>
2021-10-28 19:48:28 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var firstName= "Anamika"
+console.log (firstName)
+
+var lastName = " Arun"
+console.log (lastName)
+
+var fullName= firstName.concat(lastName);
+console.log (fullName)
+
+var a = 196
